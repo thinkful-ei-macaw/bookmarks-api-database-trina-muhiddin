1c64859093d10d69f11e4a9b7eb521d54c5c014f
Muhiddin Kurbonov <muhiddin.m.kurbonov@gmail.com>
2020-04-02 11:30:59 -0700

    first commit

---
+++ seeds/seed.bookmarks_api.sql
@@ -0,0 +1,23 @@
+INSERT INTO bookmarks_api(id, title, url, description, rating)
+VALUES
+(
+  1,
+  'Google',
+  'https://google.com',
+  'A search engine.',
+  4
+),
+( 
+  2,
+  'Thinkful',
+  'https://thinkful.com',
+  'Online coding bootcamp.',
+  5
+),
+(
+  3,
+  'Ebay',
+  'https://ebay.com',
+  'Buy sell trade.',
+  4
+);
